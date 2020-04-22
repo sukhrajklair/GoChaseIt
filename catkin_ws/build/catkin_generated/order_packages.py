d002cd57c0c3ec7812bea55d9f057369124a6c37
Sukhraj klair <sukhrajklair@gmail.com>
2020-04-22 07:45:12 -0700

    added image_process node

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/robond/Desktop/workspace/GoChaseIt/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/robond/Desktop/workspace/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/robond/Desktop/workspace/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/robond/Desktop/workspace/GoChaseIt/catkin_ws/devel;/home/robond/Desktop/workspace/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/robond/Desktop/workspace/GoChaseIt/catkin_ws/devel;/home/robond/Desktop/workspace/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -0,0 +1,77 @@
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include "sensor_msgs/Image.h"
+
+class SubAndCall 
+{
+ private:
+  ros::NodeHandle n;
+  ros::ServiceClient client;
+  ros::Subscriber sub;
+  void drive_robot(float lin_x, float ang_z)
+  {
+    ball_chaser::DriveToTarget cmnd;
+    cmnd.request.linear_x = lin_x;
+    cmnd.request.angular_z = ang_z;
+
+    client.call(cmnd);  
+  }
+  void process_image_callback(sensor_msgs::Image const img)
+  {
+    int white_pixel = 255;
+    
+    int height = img.height;
+    int width = img.step;
+
+    int left = width/3;
+    int right = 2*width/3;
+    
+    int col = 0;
+
+    for(int px=0; px<sizeof(img.data); px++)
+    { 
+      
+      if (img.data[px] == white_pixel)
+        {
+          col = px%width;
+          if (col<left)
+          {
+            //drive to left
+            this->drive_robot(0.0, 0.5);          
+          }
+          else if (col>right)
+          {
+            //drive to right
+            this->drive_robot(0.0, -0.5);          
+          }
+          else
+          { 
+            //drive straight
+            this->drive_robot(0.5, 0.0);          
+          }
+          //exit the inner loop is a white pixel is found 
+          break;
+        }
+    }
+    this->drive_robot(0.0, 0.0);  
+  }
+
+public:
+  SubAndCall()
+  {
+    client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+    sub = n.subscribe("/camera/rgb/image_raw",10, &SubAndCall::process_image_callback,this);
+  }
+};
+
+int main(int argc, char** argv)
+{
+
+    ros::init(argc, argv, "process_image");
+    
+    SubAndCall processImage;
+
+    ros::spin();
+    
+    return 0;
+}
